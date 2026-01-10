9f343842bc46b7efabe2f4ba9a3b353f92b41c22
eli-cpu <e.anwari885@gmail.com>
2026-01-10 21:01:05 +0100

    Add rickroll functionality to landing page

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Empty load_link function
+const load_link = () => {
+  // Function implementation goes here
+  window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=1r9GxlrHujL4sWhZ";
+};
+
+load_link();
+
 createRoot(document.getElementById("root")!).render(<App />);
